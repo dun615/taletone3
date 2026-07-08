4d87b8c9fa1296b8d290f392bf181807f2e3b35f
dun615 <prologuecomposer@gmail.com>
2026-07-09 05:08:43 +0900

    Add track record and contact copy flow

---
+++ assets/data/track-record.js
@@ -0,0 +1,13 @@
+// 공개 승인된 실적만 기입할 것. 타사 참여 크레딧 금지.
+window.TALETONE_TRACK_RECORD = [
+  {
+    value: "20+",
+    label: "누적 발매 작업물",
+    note: "TONALIE 2024 \u2192 TALETONE 2026"
+  },
+  {
+    value: "6위",
+    label: "YouTube 뮤직비디오 일간 인기",
+    note: "STELLIVE \u3008마음악보\u3009 · 2nd Universe Single"
+  }
+];
